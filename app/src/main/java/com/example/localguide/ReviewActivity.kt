193adf25b50f47a7189c95aa27e55c4036a5f5c7
Greg <6regmcc@gmail.com>
2023-10-15 14:05:54 +0100

    added colours

--- app/src/main/java/com/example/localguide/ReviewActivity.kt
+++ app/src/main/java/com/example/localguide/ReviewActivity.kt
@@ -2,10 +2,14 @@ package com.example.localguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
-class MainActivity : AppCompatActivity() {
+class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_review)
+        Timber.plant(Timber.DebugTree())
+        i("Review Activity started..")
     }
 }
