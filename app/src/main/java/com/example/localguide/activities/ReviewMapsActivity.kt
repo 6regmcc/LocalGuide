bb5cf2327b359120fe322fe5e8cb339b8a9f62ee
Greg <6regmcc@gmail.com>
2023-10-22 16:37:00 +0100

    updated map activity

--- app/src/main/java/com/example/localguide/activities/ReviewMapsActivity.kt
+++ app/src/main/java/com/example/localguide/activities/ReviewMapsActivity.kt
@@ -3,16 +3,69 @@ package com.example.localguide.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.localguide.databinding.ActivityReviewMapsBinding
-class ReviewMapsActivity : AppCompatActivity(){
+import com.example.localguide.databinding.ContentReviewMapsBinding
+import com.example.localguide.main.MainApp
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class ReviewMapsActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityReviewMapsBinding
+    private lateinit var contentBinding: ContentReviewMapsBinding
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        app = application as MainApp
         binding = ActivityReviewMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentReviewMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.reviews.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
     }
 }
