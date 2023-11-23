308e254fa3c2742176a9be2c9e83fbe24217db76
Greg <6regmcc@gmail.com>
2023-11-23 20:49:09 +0000

    finished refacotring

--- app/src/main/java/com/example/localguide/views/login/LoginView.kt
+++ app/src/main/java/com/example/localguide/views/login/LoginView.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.example.localguide.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import com.example.localguide.main.MainApp
 import com.example.localguide.views.register.RegisterView
@@ -18,15 +19,16 @@ import timber.log.Timber.Forest.i
 class LoginView : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var auth: FirebaseAuth
-    lateinit var app: MainApp
+    //private lateinit var auth: FirebaseAuth
+    //lateinit var app: MainApp
+    private lateinit var presenter: LoginPresenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        auth = Firebase.auth
-        app = application as MainApp
-
+        //auth = Firebase.auth
+        //app = application as MainApp
+        presenter = LoginPresenter(this)
         val email = intent.getStringExtra("email")
         binding.userEmailInput.setText(email)
 
@@ -54,7 +56,7 @@ class LoginView : AppCompatActivity() {
                     val email: String = binding.userEmailInput.text.toString().trim {it <= ' '}
                     val password: String = binding.userPasswordInput.text.toString().trim {it <= ' '}
 
-                    signIn(email, password)
+                    presenter.signIn(email, password)
                 }
 
 
@@ -69,48 +71,9 @@ class LoginView : AppCompatActivity() {
 
     }
 
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    i(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    i("User id is ${user?.uid}")
-                    i("User email is ${user?.email}")
 
-                    Toast.makeText(
-                        baseContext,
-                        "Sign successful.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    i(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    //updateUI(null)
-                }
-            }
-        // [END sign_in_with_email]
-    }
 
-    private fun updateUI(user: FirebaseUser?) {
-        val intent = Intent(this@LoginView, ReviewListView::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        intent.putExtra("user_id", user?.uid)
-        intent.putExtra("email", user?.email)
-        startActivity(intent)
-        finish()
-    }
 
-    companion object {
-        private const val TAG = "EmailPassword"
-    }
+
 
 }
