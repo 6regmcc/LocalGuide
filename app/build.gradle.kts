cad2afc98f08c10cb9e0766ee9cc24848392c915
Greg <6regmcc@gmail.com>
2023-11-26 11:16:46 +0000

    updated build.gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("kotlin-parcelize")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     id("com.google.gms.google-services")
+    id("androidx.navigation.safeargs")
 
 
 
@@ -54,11 +55,11 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,14 +69,14 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.0")
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7")
-    implementation("com.google.android.material:material:1.9.0")
-    val nav_version = "2.7.5"
-    val navigationVersion = "2.4.1"
+
+
+
+
+    val navigationVersion = "2.7.5"
 
 
     implementation ("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
-    implementation ("androidx.preference:preference:1.2.0")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation ("androidx.preference:preference:1.2.1")
 }
